ad59310071dffedba485d166cdf0ecc50a27ae84
GrewUp <gromov.zhoraeg@mail.ru>
2020-05-29 10:40:28 +0300

    Update project

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
-from langdetect import detect
 from translate import Translator
 import apiai,json,configparser, requests, discord # Import necessary libraries
-settings = configparser.ConfigParser() 
+
 config = configparser.ConfigParser()        # Creating configs readers
 
 config.read("config.ini")                   # Read configs from files
@@ -80,13 +79,14 @@ class BotDiscord(discord.Client):
             return translator.translate(q)
         return q
 
+
     
 
 
     #Discord function
     async def on_typing(self,channel,user,when):
         pass
-
+ 
     async def on_ready(self):
         print("ready")
         
@@ -107,56 +107,6 @@ class BotDiscord(discord.Client):
             await channel.send("Замолчи, пожалуйста, "+message.author.name+ ", хватит спамить!")
         await channel.send(self.response(ms))
 
-
-        '''if set("йцукенгшщзхъфывапролджэячсмитьбю").isdisjoint(ms.lower()) == True:  #Fast ru checker
-            ms = translator.translate(message.content)
-        print(settings["Chat"]["is_translate"])
-        if settings["Chat"]["is_translate"] == "True":
-            await channel.send(ms)
-        
-        if ms.lower().rfind("!дата") != -1:
-            response = requests.get("http://worldtimeapi.org/api/timezone/Europe/Moscow")
-            await channel.send(str(response.json()["datetime"])[0:10])
-        elif ms.lower().rfind("!время") != -1:
-            response = requests.get("http://worldtimeapi.org/api/timezone/Europe/Moscow")
-            await channel.send(str(response.json()["datetime"])[11:19])
-        elif ms.lower().rfind("!помощь") != -1:
-            await channel.send("""
-            Список моих комманд:
-              !время ----------- узнать время
-              !дата ------------ узнать дату
-              !помощь ---------- для получения помощи
-              !перевод --------- для включения и отключения автопереводчика
-              !чат ------------- для включения и отключения автоответчика
-            """)
-        elif ms.lower().rfind("!перевод") != -1:
-            if settings["Chat"]["is_translate"] == "True":
-                settings["Chat"]["is_translate"] = "False"
-                await channel.send("Перевод отключен")
-            else:
-                settings["Chat"]["is_translate"] = "True"
-                await channel.send("Перевод включен")
-        elif ms.lower().rfind("!чат") != -1:
-            if settings["Chat"]["is_chat"] == "True":
-                settings["Chat"]["is_chat"] = "False"
-                await channel.send("Ответчик отключен")
-            else:
-                settings["Chat"]["is_chat"] = "True"
-                await channel.send("Ответчик включен")
-        else:
-            if settings["Chat"]["is_chat"] == "True":
-                request = apiai.ApiAI(APIKEY).text_request()
-                request.lang = 'ru' 
-                print(ms)
-                request.query = ms
-                responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-                response = responseJson['result']['fulfillment']['speech']
-                
-                if response.__len__() > 0:
-                    await channel.send(response)
-                else:
-                    channel.send("Извини, я тебя не понимаю введи !помощь для ознакомления с моими возможностями")
-           '''
    
 
 # Run bot with token TOKEN
