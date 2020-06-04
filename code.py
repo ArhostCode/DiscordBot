b9dbec196fe2aeb47cd77618c5d9d9963b840daf
GrewUp <gromov.zhoraeg@mail.ru>
2020-06-04 19:14:24 +0300

    Optimizing code in code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-from translate import Translator
+from googletrans import Translator
 import apiai,json,configparser, requests, discord # Import necessary libraries
 
 config = configparser.ConfigParser()        # Creating configs readers
@@ -8,18 +8,15 @@ config.read("config.ini")                   # Read configs from files
 APIKEY = config["DialogFlow"]["api_key"]
 TOKEN = config["Discord"]["token"]          # Set Discord Token an DialogFlow apikey from files
 
-translator = Translator(to_lang="ru")       # Creating Translator
 
-
-
-
-#Bot class
+#Discord Bot class
 class BotDiscord(discord.Client):
-    
-   
 
     isTranslate = False
     isChatEnable = True
+
+    def __init__(self):
+        translator = Translator()         # Creating Translator
 
     def getDialogFlowAnswer(self,q:str):
         request = apiai.ApiAI(APIKEY).text_request()
@@ -30,7 +27,7 @@ class BotDiscord(discord.Client):
         if response.__len__() > 0:
             return response
         else:
-            return "Извини, я тебя не понимаю введи !помощь для ознакомления с моими возможностями"
+            return "Извини, я тебя не понимаю введи \"!помощь\" для ознакомления с моими возможностями"
            
 
     def getDate(self):
@@ -62,12 +59,13 @@ class BotDiscord(discord.Client):
         resp = ""
         q = self.translate(q)
         
-        if q.split(" ")[0].lower() in self.command_list:
+        if q.split(" ")[0].lower() in self.command_list:            #Check command in message
             resp += self.command_list.get(q.split(" ")[0])(self)
             q = str(q.split(" ")[1:])
+
         if self.isTranslate:
-            resp += "\n"+ q
-        print(q)
+            resp += "\n"+ q                                         #Send translated message if on translator function
+        
         if q.__len__()>5:
             if self.isChatEnable:
                 resp+=self.getDialogFlowAnswer(q)
@@ -75,23 +73,21 @@ class BotDiscord(discord.Client):
 
    
     def translate(self,q:str):
-        if set("йцукенгшщзхъфывапролджэячсмитьбю").isdisjoint(q.lower()) == True:  #Fast ru checker
-            return translator.translate(q)
+        if set("йцукенгшщзхъфывапролджэячсмитьбю").isdisjoint(q.lower()) == True:  #Fast lang checker
+            return translator.translate([q], dest='ru')
         return q
 
 
-    
-
 
     #Discord function
-    async def on_typing(self,channel,user,when):
+    async def on_typing(self,channel,user,when):        #Starts when any user typing
         pass
  
-    async def on_ready(self):
-        print("ready")
+    async def on_ready(self):                           #Starts when bot connected to Discord
+        print("Ready")
         
     
-    async def on_message(self,message):
+    async def on_message(self,message):                 #Starts when any user send message to chat
 
         
         # Check: author is bot?
@@ -102,7 +98,7 @@ class BotDiscord(discord.Client):
         channel = message.channel
         ms = message.content
         
-
+        #Check length message
         if message.content.__len__()>250:
             await channel.send("Замолчи, пожалуйста, "+message.author.name+ ", хватит спамить!")
         await channel.send(self.response(ms))
